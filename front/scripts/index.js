878efe0b6bdded2fdeb447c053eaf78f0bb1f1b6
cozazu <aranzazuarcila@gmail.com>
2024-02-09 00:03:41 -0500

    Avance con páginas y solución de AJAX

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,89 +1,4 @@
-function addMovie({
-  title,
-  year,
-  director,
-  duration,
-  genre = [],
-  rate,
-  poster,
-}) {
-  const moviecard = document.createElement("div");
-  moviecard.className = "card"; // Agregar la clase 'card' de Bootstrap
-
-  const img = document.createElement("img");
-  img.src = poster;
-  img.className = "card-img-top"; // Agregar la clase 'card-img-top' de Bootstrap
-
-  const body = document.createElement("div");
-  body.className = "card-body"; // Agregar la clase 'card-body' de Bootstrap
-
-  const mtitle = document.createElement("h5");
-  mtitle.textContent = title;
-  mtitle.className = "card-title"; // Agregar la clase 'card-title' de Bootstrap
-
-  const myear = document.createElement("p");
-  myear.textContent = `Año: ${year}`;
-  myear.className = "card-text"; // Agregar la clase 'card-text' de Bootstrap
-
-  const mdirector = document.createElement("p");
-  mdirector.innerHTML = `Director: ${director}`;
-  mdirector.className = "card-text"; // Agregar la clase 'card-text' de Bootstrap
-
-  const mduration = document.createElement("p");
-  mduration.textContent = `Duración: ${duration}`;
-  mduration.className = "card-text"; // Agregar la clase 'card-text' de Bootstrap
-
-  const mgenre = document.createElement("p");
-  mgenre.textContent = `Género: ${genre.join(", ")}`;
-  mgenre.className = "card-text"; // Agregar la clase 'card-text' de Bootstrap
-
-  const mrate = document.createElement("p");
-  mrate.textContent = `Valoración: ${rate} ⭐`;
-  mrate.className = "card-text"; // Agregar la clase 'card-text' de Bootstrap
-
-  body.appendChild(mtitle);
-  body.appendChild(myear);
-  body.appendChild(mdirector);
-  body.appendChild(mduration);
-  body.appendChild(mgenre);
-  body.appendChild(mrate);
-
-  moviecard.appendChild(img);
-  moviecard.appendChild(body);
-
-  return moviecard;
-}
-
-
-/*function renderMovies() {
-  const arraymovies = tempData.map((pelicula) => addMovie(pelicula));
-  arraymovies.forEach((element) => {
-    principalContent.appendChild(element);
-  });
-}*/
-
-/* window.addEventListener("DOMContentLoaded", renderMovies); */
-
-/* document.addEventListener('DOMContentLoaded', function () {
-    const contenedorPeliculas = document.getElementById("peliculasBox");
-
-    tempData.forEach((pelicula) => {
-        const elementosPelicula = document.createElement("div");
-        elementosPelicula.innerHTML = `
-            <h2>${pelicula.title}</h2>
-            <p>Year: ${pelicula.year}</p>
-            <p>Director: ${pelicula.director}</p>
-            <p>Duration: ${pelicula.duration}</p>
-            <p>Genre: ${pelicula.genre}</p>
-            <p>Rate: ${pelicula.rate}</p>
-            <img src="${pelicula.poster}" alt="${pelicula.title}"></img>            
-        `;
-        contenedorPeliculas.appendChild(elementosPelicula);
-    });
-}); */
-
-
-/* console.log(tempData);
+console.log(tempData);
 
 console.log(tempData);
 
@@ -134,13 +49,22 @@ function addMovie({
   moviecart.appendChild(infomovie);
 
   return moviecart;
-}
+};
+
+renderMovies();
+
+/* function renderMovies() {
+  const arraymovies = tempData.map((pelicula) => addMovie(pelicula));
+  arraymovies.forEach((element) => {
+    principalContent.appendChild(element);
+  });
+} */
 
 /*const arraymovies = tempData.map((pelicula) => addMovie(pelicula));*/
 
-/* function renderMovies() {
+function renderMovies() {
   $.get(url, (movies, status) => {
-    if (status === "succes") {
+    if (status === "success") {
       const arraymovies = movies.map((pelicula) => addMovie(pelicula));
       arraymovies.forEach((element) => {
         principalContent.appendChild(element);
@@ -155,7 +79,27 @@ function addMovie({
   });
 }
 
-renderMovies(); */ 
+
+
+/* window.addEventListener("DOMContentLoaded", renderMovies); */
+
+/* document.addEventListener('DOMContentLoaded', function () {
+    const contenedorPeliculas = document.getElementById("peliculasBox");
+
+    tempData.forEach((pelicula) => {
+        const elementosPelicula = document.createElement("div");
+        elementosPelicula.innerHTML = `
+            <h2>${pelicula.title}</h2>
+            <p>Year: ${pelicula.year}</p>
+            <p>Director: ${pelicula.director}</p>
+            <p>Duration: ${pelicula.duration}</p>
+            <p>Genre: ${pelicula.genre}</p>
+            <p>Rate: ${pelicula.rate}</p>
+            <img src="${pelicula.poster}" alt="${pelicula.title}"></img>            
+        `;
+        contenedorPeliculas.appendChild(elementosPelicula);
+    });
+}); */
 
 
 
